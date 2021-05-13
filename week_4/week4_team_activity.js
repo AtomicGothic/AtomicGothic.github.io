90b36f283cf40cb70399d6fb1f8b74abcd59ea9f
Adam Goff <atomicgothic@protonmail.com>
2021-05-13 09:19:30 -0400

    Team Activity 4
    Added the week 4 team activity code.

---
+++ week_4/week4_team_activity.js
@@ -0,0 +1,26 @@
+const divBoard = document.querySelector('.grid');
+const resetButton = document.getElementById('reset');
+const player1 = 'X';
+const player2 = 'O';
+let player = player1;
+function addPiece(e) {
+  console.log(e.target);
+  e.target.innerHTML = player;
+  if (player === player1) player = player2;
+  else player = player1;
+}
+
+// div version
+function resetBoardDiv() {
+  const divBoard = document.querySelector('.grid');
+  for (let i = 0; i < divBoard.children.length; i++) {
+    divBoard.children[i].innerText = '';
+  }
+  const children = Array.from(divBoard.children);
+  const empty = children.filter(function(child) {
+    return child.innerText == 'X' || child.innerText == 'O';
+  });
+  console.log(empty);
+}
+divBoard.addEventListener('click', addPiece);
+reset.addEventListener('click', resetBoardDiv);
