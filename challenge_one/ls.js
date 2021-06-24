a2aebe98b2109f0bf0098f8fd2a1f5f42d62c9c8
Adam Goff <atomicgothic@protonmail.com>
2021-06-23 22:12:11 -0400

    Team activity and project 2 update

--- challenge_one/ls.js
+++ challenge_one/ls.js
@@ -40,4 +40,3 @@ export function getFromLocalStorage() {
       toDoObj.renderToDos(toDoObj.getToDoList());
     }
   }
-  
---
+++ challenge_two/pantry.js
@@ -0,0 +1,13 @@
+//local private variables
+const myPantryForm = document.querySelector('.myPantryForm')
+
+let myPantry = [];
+
+class Pantry{
+    //getters
+
+    getMyPantryList() {
+        return myPantry;
+    }
+}
+//end of class Pantry
